94b007252b82085caae8787f1dc9cd4f4d3727b8
LKevin08 <rene.bueso@unah.hn>
2023-02-24 21:32:37 -0600

    Agregue los formulario
    formulario

--- KevinLopez/KevinLopez/PrimerFormulario.cs
+++ KevinLopez/KevinLopez/PrimerFormulario.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace KevinLopez
 {
-    public partial class Form1 : Form
+    public partial class PrimerFormulario : Form
     {
-        public Form1()
+        public PrimerFormulario()
         {
             InitializeComponent();
         }
